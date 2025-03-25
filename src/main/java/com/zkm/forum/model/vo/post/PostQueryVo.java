d4b1e3b9f802720e945b95947f5100e107c2b373
ZhangKaiming <ZhangKaiming.qq.com>
2025-03-25 17:54:36 +0800

    帖子模块

---
+++ src/main/java/com/zkm/forum/model/vo/post/PostQueryVo.java
@@ -0,0 +1,24 @@
+package com.zkm.forum.model.vo.post;
+
+import com.zkm.forum.common.PageRequest;
+import com.zkm.forum.model.entity.User;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+@Data
+public class PostQueryVo extends PageRequest implements Serializable {
+    private String title;
+    private String content;
+    private String tags;
+    private Integer thumbNum;
+    private Integer favourNum;
+    private User author;
+    private Date createTime;
+    private Date updateTime;
+    private Integer status;
+    private String article_abstract;
+    private Integer is_top;
+    private Integer is_featured;
+    private String original_url;
+}
