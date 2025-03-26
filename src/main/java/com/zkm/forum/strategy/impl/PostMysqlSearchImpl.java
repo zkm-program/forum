752cb5ce87127b56d9999e089e8e6bdec89c6991
ZhangKaiming <ZhangKaiming.qq.com>
2025-03-26 19:42:08 +0800

    帖子模块

--- src/main/java/com/zkm/forum/strategy/impl/PostMysqlSearchImpl.java
+++ src/main/java/com/zkm/forum/strategy/impl/PostMysqlSearchImpl.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 
 import com.zkm.forum.constant.CommonConstant;
 import com.zkm.forum.mapper.PostMapper;
+import com.zkm.forum.mapper.UserMapper;
 import com.zkm.forum.model.entity.Post;
 import com.zkm.forum.model.vo.post.PostSearchVo;
 import com.zkm.forum.strategy.SearchStrategy;
@@ -13,6 +14,8 @@ import javax.annotation.Resource;
 import java.util.ArrayList;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Service("postMysqlSearchImpl")
 public class PostMysqlSearchImpl implements SearchStrategy {
 
@@ -20,9 +23,12 @@ public class PostMysqlSearchImpl implements SearchStrategy {
     PostMapper postMapper;
     @Resource
     CommonConstant constant;
+    @Resource
+    UserMapper userMapper;
+
     @Override
     public List<PostSearchVo> searchPost(String keyWords) {
-        if(keyWords.isBlank()){
+        if (keyWords.isBlank()) {
             return new ArrayList<>();
         }
         QueryWrapper<Post> postSearchVoQueryWrapper = new QueryWrapper<>();
@@ -30,36 +36,53 @@ public class PostMysqlSearchImpl implements SearchStrategy {
                 .or()
                 .eq("content", keyWords);
         List<Post> posts = postMapper.selectList(postSearchVoQueryWrapper);
-        posts.stream().map(post -> {
-            PostSearchVo postSearchVo = new PostSearchVo();
-            boolean lowerCase=true;
-            String content = post.getContent();
-            int keyIndex = content.indexOf(keyWords.toLowerCase());
-            if (keyIndex==-1){
-                keyIndex = content.indexOf(keyWords.toUpperCase());
-                if (keyIndex!=-1){
-                    lowerCase=false;
+        return posts.stream().map(post -> {
+                    PostSearchVo postSearchVo = new PostSearchVo();
+                    boolean lowerCase = true;
+                    String content = post.getContent();
+                    int keyIndex = content.indexOf(keyWords.toLowerCase());
+                    if (keyIndex == -1) {
+                        keyIndex = content.indexOf(keyWords.toUpperCase());
+                        if (keyIndex != -1) {
+                            lowerCase = false;
+                        }
+                    }
+                    if (keyIndex != -1) {
+                        int preIndx = keyIndex > 15 ? keyIndex - 15 : 0;
+                        String preString = post.getContent().substring(preIndx, keyIndex);
+                        int lastIndex = (post.getContent().length() - keyIndex) > 35 ? keyIndex + 35 : post.getContent().length();
+                        String lastString = post.getContent().substring(keyIndex, lastIndex);
+                        if (lowerCase) {
+                            postSearchVo.setContent((preString + lastString).replaceAll(keyWords.toLowerCase(), constant.PRE_TAG + keyWords.toLowerCase() + constant.POST_TAG));
+                        } else {
+                            postSearchVo.setContent((preString + lastString).replaceAll(keyWords.toUpperCase(), constant.PRE_TAG + keyWords.toUpperCase() + constant.POST_TAG));
+                        }
+                    } else {
+                        return null;
+                    }
+                    lowerCase = true;
+                    int key = post.getTitle().indexOf(keyWords.toLowerCase());
+                    if (key == -1) {
+                        key = post.getTitle().indexOf(keyWords.toUpperCase());
+                        if (key != -1) {
+                            lowerCase = false;
+                        }
+                    }
+                    if (key != -1) {
+                        if (lowerCase) {
+                            postSearchVo.setTitle(post.getTitle().replaceAll(keyWords.toLowerCase(), constant.PRE_TAG + keyWords.toLowerCase() + constant.POST_TAG));
+                        } else {
+                            postSearchVo.setTitle(post.getTitle().replaceAll(keyWords.toUpperCase(), constant.PRE_TAG + keyWords.toUpperCase() + constant.POST_TAG));
+                        }
+                    }
+
+                    postSearchVo.setFavourNum(post.getFavourNum());
+                    postSearchVo.setThumbNum(post.getThumbNum());
+                    postSearchVo.setUpdateTime(post.getUpdateTime());
+                    return postSearchVo;
                 }
-            }
-            if (keyIndex!=-1){
-                int preIndx=keyIndex>15?keyIndex-15:0;
-                String preString=post.getContent().substring(preIndx,keyIndex);
-                int lastIndex=(post.getContent().length()-keyIndex)>35?keyIndex+35:post.getContent().length();
-                String lastString=post.getContent().substring(keyIndex,lastIndex);
-                if(lowerCase){
-                    postSearchVo.setContent((preString+lastString).replaceAll(keyWords.toLowerCase(), constant.PRE_TAG + keyWords.toLowerCase() + constant.POST_TAG));
-                }else {
-                    postSearchVo.setContent((preString+lastString).replaceAll(keyWords.toUpperCase(), constant.PRE_TAG + keyWords.toUpperCase() + constant.POST_TAG));
-                }
-            }else {
-                return null;
-            }
-
-                }
 
 
-
-        )
-        return List.of();
+        ).toList();
     }
 }
